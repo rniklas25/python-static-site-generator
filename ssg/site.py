37449ba4c98835d3bd194250b6d505fe1cf63e5f
Tom Bell <tjbell@gmail.com>
2020-04-30 14:40:20 -0600

    docs: published

---
+++ ssg/site.py
@@ -0,0 +1,10 @@
+import pathlib from Path
+
+
+class Site
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+    def create_dir(self, path):
+            directory = self.dest / path.relative_to(self.source)
+            directory.mkdir(parents=True, exist_ok=True)

340168df0bad1ca1518b289e92f4dcb460f2a1e7
Ryan Niklas <rniklas25@gmail.com>
2022-08-06 17:23:57 -0400

    docs: published

--- ssg/site.py
+++ ssg/site.py
@@ -13,4 +13,4 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.creat_dir(path)
+                self.create_dir(path)

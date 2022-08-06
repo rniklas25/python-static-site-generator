901c9fc3631e295142326989fb68537fecf0ab3c
Ryan Niklas <rniklas25@gmail.com>
2022-08-06 17:11:59 -0400

    docs: published

--- ssg/site.py
+++ ssg/site.py
@@ -8,3 +8,6 @@ class Site:
     def create_dir(self, path):
             directory = self.dest / path.relative_to(self.source)
             directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)

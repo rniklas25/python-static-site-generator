d5b50a06e6b13ba3113ad8d0bf12db29fdc3dc76
Ryan Niklas <rniklas25@gmail.com>
2022-08-06 17:09:41 -0400

    docs: published

--- ssg/site.py
+++ ssg/site.py
@@ -1,7 +1,7 @@
-import pathlib from Path
+from pathlib import Path
 
 
-class Site
+class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)

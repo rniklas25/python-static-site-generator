086530deca597a60c8ddbc3f670bdaf0c5503749
Ryan Niklas <rniklas25@gmail.com>
2022-08-08 11:41:27 -0400

    docs: published

--- ssg/site.py
+++ ssg/site.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 class Site:
-    def __init__(self, source, dest, parsers):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []

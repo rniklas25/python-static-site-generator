117ceffce3db18c4cc685939b7fbabfdf4f89210
Ryan Niklas <rniklas25@gmail.com>
2022-08-08 16:48:16 -0400

    docs: published

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -25,6 +25,7 @@ class Parser:
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
+
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
